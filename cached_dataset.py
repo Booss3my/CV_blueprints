a8602b48fefe0121082c3573934004d45810283d
Bastamy Oussama <oussama.bastamy@bordeaux-inp.fr>
2023-08-15 14:37:06 +0200

    toggle cache parameter

--- cached_dataset.py
+++ cached_dataset.py
@@ -15,34 +15,45 @@ class CachedDataset(Dataset):
 
     """
 
-    def __init__(self, image_paths, labels, size=224):
+    def __init__(self, image_paths, labels=None, size=224, cache=True):
 
         self.image_paths = image_paths
         self.labels = labels
+        self.cache = cache
+        self.use_cache = False
         self.resize_ts = transforms.Compose([
             transforms.Resize(int(size*1.2), antialias=True),
             transforms.CenterCrop(size),
         ])
+
+        if cache:
+            self.__init_cache__(size)
+
+    def __init_cache__(self, size):
         # caching (adapt to your needs)
-        nb_samples = len(image_paths)*size*size*3
+        nb_samples = len(self.image_paths)*size*size*3
         shared_array_base = mp.Array(ctypes.c_float, nb_samples)
         shared_array = np.ctypeslib.as_array(shared_array_base.get_obj())
-        shared_array = shared_array.reshape(len(image_paths), 3, size, size)
+        shared_array = shared_array.reshape(
+            len(self.image_paths), 3, size, size)
         self.shared_array = torch.from_numpy(shared_array)
-        self.use_cache = False
 
     def __len__(self):
         return len(self.image_paths)
 
     def __getitem__(self, index):
-        if self.use_cache == False:
+        if self.use_cache == False or self.cache == False:
             out = torchvision.io.read_image(self.image_paths[index])
             out = self.resize_ts(out)/255
-            self.shared_array[index] = out
+
+            if self.cache:
+                self.shared_array[index] = out
         else:
             out = self.shared_array[index]
 
         # other transforms
+        if self.labels is None:
+            return out
         return out, self.labels[index]
 
     def set_use_cache(self, value):  # set to true after the first epoch
